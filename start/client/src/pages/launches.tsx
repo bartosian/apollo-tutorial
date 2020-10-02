fc67bb08992f0fb6a7d2d9b84beb988a23cec45c
bartosian <bartosian1989@gmail.com>
2020-10-02 15:10:29 -0400

    Added hooks to apollo client

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,27 +1,27 @@
-import React, { Fragment }  from 'react';
-import { RouteComponentProps } from '@reach/router';
-import { gql } from '@apollo/client'
+import React, { Fragment } from 'react';
+import { gql, useQuery } from '@apollo/client';
 
-export const LAUNCH_TILE_DATA = gql`
-  fragment LaunchTile on Launch {
-    __typename
-    id
-    isBooked
-    rocket {
-      id
-      name
-    }
-    mission {
-      name
-      missionPatch
+import { LaunchTile, Header, Button, Loading } from '../components';
+import { RouteComponentProps } from '@reach/router';
+import * as GetLaunchListTypes from './__generated__/GetLaunchList';
+
+const GET_LAUNCHES = gql`
+  query launchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        id
+        isBooked
+        rocket {
+          id
+          name
+        }
+        mission {
+          name
+          missionPatch
+        }
+      }
     }
   }
 `;
-
-interface LaunchesProps extends RouteComponentProps {}
-
-const Launches: React.FC<LaunchesProps> = () => {
-  return <div />;
-}
-
-export default Launches;
